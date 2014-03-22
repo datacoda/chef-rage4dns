af43dfd9871639456ea9ebbce57401234eb9d070
Ted Chen <ted@nephilagraphic.com>
2014-03-22 01:08:19 -0300

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'rage4dns'
+maintainer       'Nephila Graphic'
+maintainer_email 'ted@nephilagraphic.com'
+license          'Apache 2.0'
+description      'Installs/Configures rage4dns'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
+
